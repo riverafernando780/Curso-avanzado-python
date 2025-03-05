e7f1f98a362a0a8983ddab7fb3d9571f653e325a
Fernando Rivera(Alienware) <riverafernando_780@proton.me>
2025-03-04 21:57:08 -0600

    lectura de matrices

--- operaciones_array.py
+++ operaciones_array.py
@@ -1,6 +1,16 @@
+import numpy as np
+
 print("Este programa permite operar con matrices de tamaño nxn")
-n=int(input("Ingrese el tamaño ''n'' de la matriz: "))
-colector=[]
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        colector.append(float(input("Ingrese el elemento: (",i,",",j,")")))
+def creaMatriz():
+    n=int(input("Ingrese el tamaño ''n'' de la matriz: "))
+    print(f"Se va crear una matriz de tamaño {n}x{n}")
+    colector=[]
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            colector.append(float(input(f"Ingrese el elemento: ({i},{j}): ")))
+    matriz=np.array(colector)
+    matriz=np.reshape(matriz,(n,n))
+    return matriz
+
+a=creaMatriz()
+print(a)
